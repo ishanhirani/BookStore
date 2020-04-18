35f713c347307b2330b7f69a9e011da0603f4f85
ishanhirani <ishanhirani123@gmail.com>
2020-04-19 01:58:58 +0530

    Initial commit

---
+++ app/src/main/java/com/ishan/bookstore/fragment/ProfileFragment.kt
@@ -0,0 +1,25 @@
+package com.ishan.bookstore.fragment
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ishan.bookstore.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+
+}
